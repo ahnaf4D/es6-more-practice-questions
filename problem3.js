e94905355ed5bf9f45066a592b22d70eb99c5275
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-23 10:37:42 +0600

    All files added

---
+++ problem3.js
@@ -0,0 +1,25 @@
+let data2 = {
+    data:
+        [
+            {
+                bookId: 1,
+                bookDetails: {
+                    name: "habluder adda",
+                    category: "XYZ",
+                    price: "20$",
+                },
+                bookCategory: "Basic",
+            },
+            {
+                bookId: 2,
+                bookDetails: {
+                    name: "gobluder adda",
+                    category: "ABC",
+                    price: "40$",
+                },
+                bookCategory: "Beginner",
+            }
+        ]
+}
+const output = data2.data[0].bookDetails.name;
+console.log(output);
